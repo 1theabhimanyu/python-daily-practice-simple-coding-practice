8fe189e36690e17ba33c5bb47b9af5d6ae768dc0
Abhimanyu Kumar <theabhimanyuofficial@gmail.com>
2026-07-14 11:55:49 +0530

    First version with complete code of chaesar chipar..

--- ChaeserChiper.py
+++ ChaeserChiper.py
@@ -1,11 +1,11 @@
 alphabet = ['a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 #take all the needs here
-YourNeed = input("What you want to do?\n For encrypt, type'encode' and For decrypt, type'decode': ")
-msg = input("Type your msg here: ")
+YourNeed = input("What you want to do?\n For encrypt, type'encode' and For decrypt, type'decode': ").lower()
+msg = input("Type your msg here: ").lower()
 ShiftNumber = int(input("Enter the shift number, you want: "))
 
-def Encode(msg, ShiftNumber):
+def encode(msg, ShiftNumber):
     final_msg = ""
     for char in msg:
         Shift_value = alphabet.index(char) + ShiftNumber
@@ -13,4 +13,16 @@ def Encode(msg, ShiftNumber):
         final_msg += alphabet[Shift_value]
     print(final_msg)
 
-Encode(msg, ShiftNumber)
+def decode(msg, ShiftNumber):
+    final_msg = ""
+    for char in msg:
+        Shift_value = alphabet.index(char) - ShiftNumber
+        Shift_value %= len(alphabet)
+        final_msg += alphabet[Shift_value]
+    print(final_msg)
+
+if YourNeed == "encode":
+    encode(msg, ShiftNumber)
+else:
+    decode(msg, ShiftNumber)
+
