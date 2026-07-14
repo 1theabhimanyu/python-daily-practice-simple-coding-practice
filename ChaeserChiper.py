aed04d0c9449e2625593b7a0e5dad8f7e038551b
Abhimanyu Kumar <theabhimanyuofficial@gmail.com>
2026-07-14 10:37:20 +0530

    first version of Chaeser Chiper

---
+++ ChaeserChiper.py
@@ -0,0 +1,16 @@
+alphabet = ['a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+#take all the needs here
+YourNeed = input("What you want to do?\n For encrypt, type'encode' and For decrypt, type'decode': ")
+msg = input("Type your msg here: ")
+ShiftNumber = int(input("Enter the shift number, you want: "))
+
+def Encode(msg, ShiftNumber):
+    final_msg = ""
+    for char in msg:
+        Shift_value = alphabet.index(char) + ShiftNumber
+        Shift_value %= len(alphabet)
+        final_msg += alphabet[Shift_value]
+    print(final_msg)
+
+Encode(msg, ShiftNumber)
